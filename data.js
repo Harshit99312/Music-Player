3cdef7f1e6595e10d9a39f5d4f5b63c4f702450a
Harshit99312 <harshitsharma99312@gmail.com>
2023-03-31 16:19:48 +0530

    Music_Player...

---
+++ data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'Song 1',
+        path: './Song 1.mp3',
+        artist: 'artist 1',
+        cover: 'images/1.png'
+    },
+    {
+        name: 'song 2',
+        path: './Song 2.mp3',
+        artist: 'artist 2',
+        cover: 'images/2.png'
+    },
+    {
+        name: 'song 3',
+        path: './Song 3.mp3',
+        artist: 'artist 3',
+        cover: 'images/3.png'
+    },
+    {
+        name: 'song 4',
+        path: './Song 4.mp3',
+        artist: 'artist 4',
+        cover: 'images/4.png'
+    },
+    {
+        name: 'song 5',
+        path: './Song 5.mp3',
+        artist: 'artist 5',
+        cover: 'images/5.png'
+    },
+]
